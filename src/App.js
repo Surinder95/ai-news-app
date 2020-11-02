418a09f05ab459e9da5fafb5edd81477532812fd
Surinder95 <surindersingh9495@gmail.com>
2020-11-01 21:11:54 -0800

    added both folders to repo

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = '4cae5b2df550a0f0032281daed8ec9dd2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if(command === "testCommand") {
+          alert("code was executed");
+        }
+      }
+    }) 
+  }, [])
+
+  return (
+    <div>
+      <h2>News By Alana</h2>
+    </div>
+  );
+}
+
+export default App;
