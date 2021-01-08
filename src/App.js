df55159a00c10a8f84f1933ccd37a66baac7bf89
Surinder95 <surindersingh9495@gmail.com>
2021-01-07 18:34:14 -0800

    developed skeletal version of UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './Components/NewsCards/NewsCards';
 
 const alanKey = '4cae5b2df550a0f0032281daed8ec9dd2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+  const [newsArticles, setNewArticles] = useState([]);
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if(command === "testCommand") {
-          alert("code was executed");
+      onCommand: ({ command, articles }) => {
+        if(command === "newHeadlines") {
+          setNewArticles(articles);
         }
       }
     }) 
@@ -20,6 +22,7 @@ const App = () => {
   return (
     <div>
       <h2>News By Alan</h2>
+      <NewsCards articles = {newsArticles} />
     </div>
   );
 }
---
+++ src/Components/NewsCard/NewsCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
+    return (
+        <Card>
+            <CardActionArea>
+                <CardMedia/>
+                <div>
+                    <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+                </div>
+                <Typography gutterBottom variant="h5"></Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary"></Button>
+                <Typography variant="h5" color="textSecondary"></Typography>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default NewsCard;
