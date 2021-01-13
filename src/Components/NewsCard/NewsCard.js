ec688c484e905080152002bad5e4a7954795c30d
Surinder95 <surindersingh9495@gmail.com>
2021-01-12 22:37:08 -0800

    display real time article data

--- src/Components/NewsCard/NewsCard.js
+++ src/Components/NewsCard/NewsCard.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import useStyles from './styles.js';
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
+    const classes = useStyles();
+    
     return (
         <Card>
             <CardActionArea>
-                <CardMedia/>
+                <CardMedia className={ classes.media } image={ urlToImage || 'https://e7.pngegg.com/pngimages/422/126/png-clipart-newspaper-computer-icons-symbol-news-icon-text-logo.png' } />
                 <div>
-                    <Typography variant="body2" color="textSecondary" component="h2"></Typography>
-                    <Typography variant="body2" color="textSecondary" component="h2"></Typography>
-                </div>
-                <Typography gutterBottom variant="h5"></Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div> 
+                <Typography gutterBottom variant="h5">{title}</Typography>
                 <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary"></Button>
-                <Typography variant="h5" color="textSecondary"></Typography>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{ i + 1 }</Typography>
             </CardActions>
         </Card>
     )
