6f1659d2aa1cec4c568256cd7e47747d3ebc5a2b
Surinder95 <surindersingh9495@gmail.com>
2021-02-04 22:06:13 -0800

    Landing Page Modules for clarity

--- src/Components/NewsCards/NewsCards.js
+++ src/Components/NewsCards/NewsCards.js
@@ -4,14 +4,32 @@ import { Grid, Typography, Grow } from '@material-ui/core';
 import useStyles from './styles.js';
 
 const infoCards = [
-    { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
-    { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
-    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
-    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+    { color: '#FC766AFF', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#5B84B1FF', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+    { color: '#D198C5FF', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
+    { color: '#E0C568FF', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
 ];
 
 const NewsCards = ({ articles }) => {
     const classes = useStyles();
+
+    if(!articles.length) {
+        return (
+            <Grow in>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((infoCard) => (
+                        <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+                            <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+                                <Typography variant="h5">{infoCard.title}</Typography>
+                                {infoCard.info ? (<Typography variant="h6">{infoCard.info}</Typography>) : null}
+                                <Typography variant="h7">Say something like "{infoCard.text}"</Typography>
+                            </div>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grow>
+        );
+    }
 
     return (
         <Grow in>
